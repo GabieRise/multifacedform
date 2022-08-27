702d86d50e4fd7743a899506432898cf83dde0db
GabieRise <merrygabby14@gmail.com>
2022-08-27 22:33:31 +0100

    validation

--- script.js
+++ script.js
@@ -7,7 +7,13 @@ const firstBackBtn = document.querySelector("#firstBackBtn")
 const secondBackBtn = document.querySelector("#secondBackBtn")
 const submitBtn = document.querySelector("#submitBtn")
 
+const fName = document.querySelector("#fName")
+const lName = document.querySelector("#lName")
+const eMail = document.querySelector("#eMail")
+const age = document.querySelector("#age")
+
 function firstNext(){
+    if(fName.value !=="" && lName.value !=="" && age.value >=20)
     perInfo.style.display="none"
     edInfo.style.display="flex"
     healthInfo.style.display="none"
@@ -15,6 +21,7 @@ function firstNext(){
 function secondNext(){
     edInfo.style.display="none"
     healthInfo.style.display="flex"
+    perInfo.style.display="none"
 
 }
 function firstBack(){
@@ -28,6 +35,7 @@ function secondBack(){
 function submit(){
     alert("Submited successfully")
 }
+
 firstNextBtn.addEventListener("click",firstNext())
 secondNextBtn.addEventListener("click", secondNext())
 firstBackBtn.addEventListener("click",firstNext)
